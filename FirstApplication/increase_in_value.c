8fb2840228de5de44869d9bb4c890dc03487d053
Ali AKBIYIK <aliiakbiyikk@gmail.com>
2023-10-27 14:18:35 +0300

    Fast value assignment and increment

---
+++ FirstApplication/increase_in_value.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i = 3;
+	printf("%d\n",i);
+	
+	i +=5; // i= i+5
+	printf("%d\n",i);
+	
+	i -=5; // i= i-5
+	printf("%d\n",i);
+	
+	i *=3; // i= i*5
+	printf("%d\n",i);
+	
+	i /=3; // i= i/3
+	printf("%d",i);
+	
+	return 0;
+}
