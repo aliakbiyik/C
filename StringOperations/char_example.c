b64c8664232ed979b512fb4963a06422cb2fcb5d
Ali AKBIYIK <aliiakbiyikk@gmail.com>
2023-10-28 02:03:38 +0300

    Char

---
+++ StringOperations/char_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	char character = 'k';
+	printf("%c\n", character);
+	
+	char message[] = "Hello world!";
+	printf("%s", message);
+	
+	return 0;
+}
